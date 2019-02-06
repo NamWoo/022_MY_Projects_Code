27f3ba00ddb190a5d61f0ef759690b99186d544a
Nam Woo <preice@yonsei.ac.kr>
2019-02-07 00:55:29 +0900

    add

--- python code/numbering.py
+++ python code/numbering.py
@@ -3,12 +3,10 @@ import re
 from os import rename, listdir
 import shutil
 
-nowt = 5
+nowt = 6
 nown = 2
 nowp = "02_Hierarchical and Density Based Clustering\\"
-# 출처: https://118k.tistory.com/303 [개발자로 살아남기]
 
-# D:\!NW_Learning\02 MOOC\004_DataScientist_Udacity\04_Unsupervised Learning\02_Hierarchical and Density Based Clustering\img
 # ![02](img/03-001.png)
 
 path = "D:\\!NW_Learning\\02 MOOC\\004_DataScientist_Udacity\\04_Unsupervised Learning\\"
@@ -16,72 +14,54 @@ path = path + nowp
 path_ini = path + 'img_ini'
 path_img = path + 'img'
 
-ini_files = listdir(path_ini)
-img_files = listdir(path_img)
+
+def ini():
+
+    ini_files = listdir(path_ini)
+    img_files = listdir(path_img)
+
+    # 숫자에 0넣기
+    if nowt < 10:
+        titling = '0' + str(nowt)
+    else:
+        titling = str(nowt)
+
+    if nown < 10:
+        numbering = '00' + str(nown)
+    else:
+        numbering = '0' + str(nown)
+        
+
+    # 옮겨질 img 폴더의 마지막 파일명 확인 후 앞서 정의한 숫자와 비교.
+    img_files_n = img_files[-1:]
+    find_naming = '^.*(?=[-])'
+    r = re.compile(find_naming)
+    r = r.match(img_files_n[0])
+    last_n = int(r.group())
+
+    if last_n != nowt:
+        pass
+    else:
+        print("img 마지막 파일명: "+ str(img_files_n[0]), "현재 정의한 목차번호: " + str(nowt))
+        exit
 
 
-# 옮겨질 img 폴더의 마지막 파일명 확인 후 앞서 정의한 숫자와 비교.
-img_files_n = img_files[-1:]
-find_naming = '^.*(?=[-])'
-r = re.compile(find_naming)
-r = r.match(img_files_n[0])
-last_n = int(r.group())
+    for i in range(1,15):
+       # print("![" + n1 + "](img/" + n1 + "-" + n2 + ".png)")
+        # print("![" + titling + "-" + numbering + "](img/" + titling + "-" + numbering + ".png)")
+        if i < 10:
+            print("![" + titling + "-00" + str(i) + "](img/" + titling + "-00" + str(i) + ".png)")
+        elif i < 100:
+            print("![" + titling + "-0" + str(i) + "](img/" + titling + "-0" + str(i) + ".png)")
 
-if last_n != nowt:
-    pass
-else:
-    print("img 마지막 파일명: "+ img_files_n, "현재 정의한 목차번호: " + str(nowt))
-    exit
-
-
-# 파일 이름 바꾸며 옮기기
-for i in nown:
-    
-       
-
-
-
-
-
-
-for i in ini_files:
-    print(i)
-
-    name = str(nowt)+str(nown)
-
-
-    i.rename(i,)
-    print(i)
-
-
-
-
-
-
-
-
-
-# print(img_files_n)
 
 # shutil.move(a,b)
-
-
-
-# n1 = '04'
-# for i in range(1,15):
-    
-#     if i < 10:
-#         n2 = '00' + str(i)
-#     elif i < 100:
-#         n2 = '0' + str(i)
-
-    
-#     # print("![" + n1 + "](img/" + n1 + "-" + n2 + ".png)")
-#     print("![" + n1 + "-" + n2 + "](img/" + n1 + "-" + n2 + ".png)")
-
-
-
+# 출처: https://118k.tistory.com/303
 
 
 if __name__ == "__main__":
-    pass
+
+    ini()
+
+
+    
