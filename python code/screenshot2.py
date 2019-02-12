73e493d0c56e23fe0798fa543db6f3a86ed6cce8
Nam Woo <preice@yonsei.ac.kr>
2019-02-12 21:34:01 +0900

    add

--- python code/screenshot2.py
+++ python code/screenshot2.py
@@ -9,18 +9,21 @@ import pyscreenshot as ImageGrab
 # https://pypi.org/project/pyscreenshot/
 # https://pyscreenshot.readthedocs.io/en/latest/
 
+# wxPython 도 있음 https://klonic.tistory.com/18
+
+
 ######### 정의하기 #########
 # 마지막 이미지 번호 이어할 것인지 새로 만들 것인지
-# option_num = 0  # 0 이면 아래 정의한 새번호로 스샷저장
-option_num = 1  # 1 이면 폴더내 마지막 파일명 이어서 스샷저장
+option_num = 0  # 0 이면 아래 정의한 새번호로 스샷저장
+# option_num = 1  # 1 이면 폴더내 마지막 파일명 이어서 스샷저장
 # option_num = 2  # 2 이면 마지막 번호까지 for문 출력
-new_num_t = 10
+new_num_t = 18
 new_num_n = 1
 
 # 폴더이름 정의
 # nowp = "03_Gaussian Mixture Models and Cluster Validation\\"
-nowp = "04_Dimensionality Reduction and PCA\\"
-# nowp = "05_Random Projection and ICA\\"
+# nowp = "04_Dimensionality Reduction and PCA\\"
+nowp = "05_Random Projection and ICA\\"
 path = "D:\\!NW_Learning\\02 MOOC\\004_DataScientist_Udacity\\04_Unsupervised Learning\\"
 ############################
 
